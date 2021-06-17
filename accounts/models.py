781e8033b3c4466fae4b9154f2534ecd0fa966ba
lyoman <leomuchenje@gmail.com>
2021-06-17 11:26:41 +0200

    firstname

--- accounts/models.py
+++ accounts/models.py
@@ -7,8 +7,8 @@ class User(AbstractUser):
     medicall_staffer = models.CharField(max_length=50, blank=True)
     specialist_area = models.CharField(max_length=50, blank=True)
     email           = models.EmailField(unique=True, blank=True)
-    last_name       = models.EmailField(blank=True)
-    first_name      = models.EmailField(blank=True)
+    last_name       = models.CharField(blank=True)
+    first_name      = models.CharField(blank=True)
     phone_regex     = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number    = models.CharField(max_length=17, blank=True) # validators should be a list
     updated         = models.DateTimeField(auto_now=True, auto_now_add=False)
